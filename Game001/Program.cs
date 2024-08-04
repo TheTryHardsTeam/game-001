e44329ab241dcacec3f7dc5dce7a5aa5eb00d6eb
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2024-08-03 21:01:19 -0500

    day1 First Program with FNA

--- Game001/Program.cs
+++ Game001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Xna.Framework;
+
+static class Program {
+  [STAThread]
+  static void Main(string[] args) {
+    using (Game game = new Game())
+    {
+      new GraphicsDeviceManager(game);
+      game.Run();
+    }
+  }
+}
